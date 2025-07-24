0e59d2d0ab516fa6ea60207c6fcc0c9d2ee7006a
付念 <fn_kobe@163.com>
2025-07-24 19:28:09 +0800

    Initial commit

---
+++ src/main/java/com/funian/algorithm/controller/Hello.java
@@ -0,0 +1,8 @@
+package com.funian.algorithm.controller;
+
+/**
+* @Auther FuNian
+* @Date 2025/7/10 16:51
+* @ClassName:Hello
+*/public class Hello {
+}
---
+++ src/main/java/com/funian/algorithm/controller/Test.java
@@ -0,0 +1,7 @@
+package com.funian.algorithm.controller;
+
+/**
+* @Auther FuNian
+* @ClassName:Test
+*/public class Test {
+}
